809f47afd3d4673a512a995cad6c7a33f5ae4635
Ahmed Samir <a.samir9710@gmail.com>
2023-08-22 17:45:15 +0300

    Added unit tests for accounts app

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -59,3 +59,28 @@ class CustomerTests(TestCase):
         response = self.client.put(self.customer_detail_url.format(id=self.customer.id))
 
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class AccountTest(TestCase):
+
+    def setUp(self) -> None:
+        self.customter = Customer.objects.create(name='Customer1', email='customer@gmail.com')
+        self.account = Account.objects.create(customer=self.customter, balance=10.00)
+
+        self.account_url = '/api/accounts/'
+        self.accounts_detail_url = '/api/accounts/{id}/'
+
+    def test_list_all_accounts(self):
+        response = self.client.get(self.account_url)
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_get_account_success_case(self):
+        response = self.client.get(self.accounts_detail_url.format(id=self.account.id))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_get_account_failure_case_not_found(self):
+        response = self.client.get(self.accounts_detail_url.format(id=100000)) # invalid id
+
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
